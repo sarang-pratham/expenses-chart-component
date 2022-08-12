cf0323f287cbda9c93c1f2e0b31f50e4c2519c70
Sarang Pratham <sarangpratham720@gmail.com>
2022-08-12 18:15:13 +0530

    added styles

--- src/App.svelte
+++ src/App.svelte
@@ -5,26 +5,52 @@
 
 <main>
   <div class="sec1">
-    <div class="balance">
-      <p>My balance</p>
-      <p>$921.48</p>
-    </div>
+    <p class="balance">My balance <br/> <span>$921.48</span></p>
     <img src={Logo} alt="logo" id="logo">
   </div>
 
   <div class="sec2">
     <h3>Spending - Last 7 days</h3>
-    <div id="bar-chart"></div>
+    <div id="bar-chart">
+      <div class="day">
+        <div class="bar"></div>
+        <p>mon</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>tue</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>wed</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>thu</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>fri</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>sat</p>
+      </div>
+
+      <div class="day">
+        <div class="bar"></div>
+        <p>sun</p>
+      </div>
+    </div>
     <hr>
     <div class="total">
-      <div class="total-1">
-        <small>Total this month</small>
-        <p>$478.33</p>
-      </div>
-      <div class="total-2">
-        <p>+2.4%</p>
-        <small>from last month</small>
-      </div>
+      <p class="total-1">Total this month <br/> <span>$478.33</span></p>
+      <p class="total-2"><span>+2.4%</span> <br/> from last month</p>
     </div>
   </div>
 </main>
