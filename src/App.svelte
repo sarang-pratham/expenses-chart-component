1defe52a88d25dba316cff95ef92c865350f0a74
Sarang Pratham <sarangpratham720@gmail.com>
2022-08-13 19:25:35 +0530

    added bar chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Logo from './assets/logo.svg'
-  // import Counter from './lib/Counter.svelte'
+  import BarChart from './lib/BarChart.svelte'
 </script>
 
 <main>
@@ -11,43 +11,8 @@
 
   <div class="sec2">
     <h3>Spending - Last 7 days</h3>
-    <div id="bar-chart">
-      <div class="day">
-        <div class="bar"></div>
-        <p>mon</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>tue</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>wed</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>thu</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>fri</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>sat</p>
-      </div>
-
-      <div class="day">
-        <div class="bar"></div>
-        <p>sun</p>
-      </div>
-    </div>
-    <hr>
+    <BarChart/>
+    <hr/>
     <div class="total">
       <p class="total-1">Total this month <br/> <span>$478.33</span></p>
       <p class="total-2"><span>+2.4%</span> <br/> from last month</p>
---
+++ src/lib/BarChart.svelte
@@ -0,0 +1,71 @@
+<script>
+  let getData = async () => {
+    let response = await fetch('../../data.json')
+    return await response.json()
+  }
+
+  let data = getData()
+
+</script>
+
+{#await data then value}
+<div id="bar-chart">
+  <div class="bar">
+    <div class="t" style="height: {value[0].amount * 2}px;"></div>
+    <span>mon</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[1].amount * 2}px;"></div>
+    <span>tue</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[2].amount * 2}px; background-color: var(--cyan);"></div>
+    <span>wed</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[3].amount * 2}px;"></div>
+    <span>thu</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[4].amount * 2}px;"></div>
+    <span>fri</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[5].amount * 2}px;"></div>
+    <span>sat</span>
+  </div>
+  <div class="bar">
+    <div class="t" style="height: {value[6].amount * 2}px;"></div>
+    <span>sun</span>
+  </div>
+</div>
+{/await}
+
+<style>
+  #bar-chart{
+  display: flex;
+  justify-content: space-between;
+  height: 12rem;
+}
+
+.bar{
+  width: 30px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: auto;
+}
+.t{
+  width: 100%;
+  background-color: var(--soft-red);
+  border-radius: 0.2rem;
+}
+.bar span{
+  text-align: center;
+  font-size: 14px;
+  margin: 0.5rem 0 1rem 0;
+  color: var(--medium-brown);
+  font-weight: 400;
+}
+</style>
